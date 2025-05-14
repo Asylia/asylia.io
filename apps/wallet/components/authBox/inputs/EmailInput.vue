3b20edb6a8d0ed1f76a51ae977667f324861d609
nomadboy20 <infodavidzita@gmail.com>
2025-05-14 14:41:34 +0200

    prepare auth layout

---
+++ apps/wallet/components/authBox/inputs/EmailInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <UInput class="w-full" :placeholder="props.placeholder" size="lg" />
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    placeholder?: string;
+  }>(),
+  {
+    placeholder: 'Enter your email',
+  },
+);
+</script>
---
+++ apps/wallet/components/authBox/inputs/PasswordInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <UInput
+    v-model="value"
+    class="w-full"
+    trailing-icon="material-symbols:visibility-rounded"
+    :placeholder="props.placeholder"
+    :type="showPassword ? 'text' : 'password'"
+    size="lg"
+    :ui="{ trailing: 'pe-1' }"
+  >
+    <template #trailing>
+      <UButton
+        color="neutral"
+        variant="link"
+        size="sm"
+        :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+        :aria-label="showPassword ? 'Hide password' : 'Show password'"
+        :aria-pressed="showPassword"
+        aria-controls="password"
+        @click="showPassword = !showPassword"
+      />
+    </template>
+  </UInput>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    placeholder?: string;
+  }>(),
+  {
+    placeholder: 'Enter your password',
+  },
+);
+
+const value = defineModel<string>();
+
+const showPassword = ref<boolean>(false);
+</script>
