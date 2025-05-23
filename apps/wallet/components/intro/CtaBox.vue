529d4d67afcf736e902e45c87960359d3bf67e1c
nomadboy20 <infodavidzita@gmail.com>
2025-05-23 10:20:10 +0200

    update cta box bg color

--- apps/wallet/components/intro/CtaBox.vue
+++ apps/wallet/components/intro/CtaBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-[240px] h-[230px] rounded-lg border-slate-800 bg-slate-800 flex flex-col items-center justify-center border hover:cursor-pointer hover:border-primary transition-all duration-200 hover:shadow-lg"
+    class="w-[240px] h-[230px] rounded-lg border-slate-800 bg-dark-secondary flex flex-col items-center justify-center border hover:cursor-pointer hover:border-primary transition-all duration-200 hover:shadow-lg"
   >
-    <span class="text-2xl text-center">
+    <span class="text-2xl text-center text-gray-100">
       <slot />
     </span>
     <FontAwesomeIcon :icon="props.icon" class="text-primary text-4xl mt-4" />
